16c3fa800607fba9b0c320802285523377110b0d
Lars Kruse <lak@praqma.net>
2015-01-09 15:26:15 +0100

    Analytics

--- javascripts/googleanalytics.js
+++ javascripts/googleanalytics.js
@@ -3,6 +3,8 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-47787341-3', 'auto');
+  ga('create', 'UA-47787341-4', 'auto');
   ga('send', 'pageview');
+
+
 
